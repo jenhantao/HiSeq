48ddf844ee3b258cf35c27a2b73960789145f741
Jenhan <jenhantao@gmail.com>
2012-09-10 09:30:59 -0700

    added some comments

--- linkSeqToChem.py
+++ linkSeqToChem.py
@@ -1,5 +1,11 @@
 import os
 path="./data"  # insert the path to the directory of interest here
+# I want to process the .his values which are just raw counts of every sequence that appears
+# I want to process the .val files which are derviced from the .proc files
+# formatting for .val files [.proc fileName] [enrichedCount] [originalCount] [enrichmentRatio]
 dirList=os.listdir(path)
+files = [];
 for fname in dirList:
-    print fname
+    files.append(fname)
+for fileName in files:
+    print(fileName)
