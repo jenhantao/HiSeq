7a949379f4a75cdef1a8be39e21447549bfa62cc
Jenhan <jenhantao@gmail.com>
2012-09-13 01:11:34 -0700

    all data gathered, now need to generate results

--- linkSeqToChem.py
+++ linkSeqToChem.py
@@ -10,7 +10,7 @@ dirList=os.listdir(path)
 #read in configuration file and store each unique chemical
 with open("./data/chempools.config") as f:
      configFile = f.readlines()
-_chemDict = dict();
+_chemDict = dict(); # stores the chemicals that are particular to each pool
 currentPool = "";
 tempChemList = list();
 for line in configFile:
@@ -24,7 +24,10 @@ for line in configFile:
           #print "adding chemical: "+line.rstrip("\n");          
           currentChemicals.append(line.rstrip("\n"))
           tempChemList.append(line.rstrip("\n"))
+del _chemDict["Enrichment"] #Enrichment is a variable, not a pool
+_chemDict[currentPool] = set(currentChemicals); #add the last entry
 _chemList = set(tempChemList) #list of unique chemicals needed for initialzing entries in _chemHash
+enrichmentThreshold = float(configFile[0].split(" ")[3]) #ratio required between new pool and original to be considered enriched
 
 
 #initialize chemHash
@@ -49,6 +52,40 @@ def addSeqEntry(sequence,chemical):
           _chemHash[sequence] = newDict;
           
 
+# read in the his files specified by the chempool.config
+
+_ratioHash = dict() #a dictionary that will hold the enrichment ratios for all of the pools (excepting for the last one, the prephage selection
+                    #key is the pool name as indicated in the config file, value is a dictionary containing the enrichment values
+poolKeys = sorted(_chemDict.keys())
+# last pool is presumed to be the pre-phage selection
+prephageHistValues = dict(); #key is sequence, value is count
+with open("./data/"+poolKeys[-1]+".his") as f:
+     hisFile = f.readlines()
+for line in hisFile:
+     if len(line)>0:
+          tokens = line.rstrip("\n").split("\t")
+          #print "seq: "+tokens[0]+" count: "+tokens[1]
+          prephageHistValues[tokens[0]]=tokens[1]
+for i in range(len(poolKeys)-1):
+     #print "reading in: "+poolKeys[i]
+     with open("./data/"+poolKeys[i]+".his") as f:
+          hisFile = f.readlines()
+     currentRatioDict = dict()
+     for line in hisFile:
+          tokens = line.rstrip("\n").split("\t")
+          if tokens[0] in prephageHistValues:
+               ratio = float(tokens[1])/float(prephageHistValues[tokens[0]])
+               if ratio >= enrichmentThreshold:
+                    currentRatioDict[tokens[0]] = ratio
+                    #print tokens[0]+"|"+str(currentRatioDict[tokens[0]])
+     _ratioHash[poolKeys[i]]=currentRatioDict
+
+# produce the data set that we're interested in, list of sequences and the chemicals that enrich them
+for key in _ratioHash:
+     print key
+     
+     
+     
 
 #testArea
 addSeqEntry("moo","Urea")
